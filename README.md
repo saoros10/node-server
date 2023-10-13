9acc60226da774084e82a82f31343786c1b75d1d
SAOO <saortizospina@gmail.com>
2023-10-13 13:27:43 -0500

    Segunda version lista de tareas, se añadio la funcion eliminar y las funciones asincronas

---
+++ README.md
@@ -0,0 +1,18 @@
+¿Qué sucedio al usar async y await?
+
+Al utilizar Asyn y Await en la funcion displayMenu(), me permite esperar que las promesas 
+se resuelvan antes de continuar con la ejecuciona del programa es decir se vuelvan Asincronas. 
+Asi mismo en cada una de las funciones de crear, eliminar, listar y modificar tareas.
+
+Para este caso no seespecifico un tiempo de espera setTimeout() por que no es la respuesta 
+de un servidor, si no la del usuario.
+
+En general nos permite manejar mejor los casos de anidamiento
+
+¿Qué sucedio al usar el método then()? y ¿Qué diferencias encontraste entre async, await y el método then()?
+
+En general hace lo mismo que el async y await solo que estos primeros parametros
+son mas faciles de escribir y de leer en el flujo del codigo
+
+
+
--- app.js
+++ app.js
@@ -8,72 +8,113 @@ const rl = readline.createInterface({
 const tasks = [];
 
 function addTask() {
-  rl.question('Descripción de la tarea: ', (description) => {
-    const task = {
-      id: tasks.length + 1,
-      description,
-      completed: false,
-    };
-    tasks.push(task);
-    console.log(`Tarea añadida. ID: ${task.id}`);
-    displayMenu();
+  return new Promise((resolve, reject) => {
+    rl.question('Descripción de la tarea: ', (description) => {
+      const task = {
+        id: tasks.length + 1,
+        description,
+        completed: false,
+      };
+      tasks.push(task);
+      console.log(`Tarea añadida. ID: ${task.id}`);
+      resolve();
+    });
   });
 }
 
 function listTasks() {
-  if (tasks.length === 0) {
-    console.log('La lista de tareas está vacía.');
-  } else {
-    console.log('Lista de tareas:');
-    tasks.forEach((task) => {
-      const status = task.completed ? 'Completada' : 'Pendiente';
-      console.log(`ID: ${task.id} - Descripción: ${task.description} - Estado: ${status}`);
-    });
-  }
-  displayMenu();
+  return new Promise((resolve, reject) => {
+    if (tasks.length === 0) {
+      console.log('La lista de tareas está vacía.');
+    } else {
+      console.log('Lista de tareas:');
+      tasks.forEach((task) => {
+        const status = task.completed ? 'Completada' : 'Pendiente';
+        console.log(`ID: ${task.id} - Descripción: ${task.description} - Estado: ${status}`);
+      });
+    }
+    resolve();
+  });
 }
 
 function markTaskAsCompleted() {
-  rl.question('ID de la tarea a marcar como completada: ', (taskId) => {
-    const task = tasks.find((t) => t.id === parseInt(taskId, 10));
-    if (task) {
-      task.completed = true;
-      console.log(`Tarea ${taskId} marcada como completada.`);
-    } else {
-      console.log('ID de tarea no encontrado.');
-    }
-    displayMenu();
+  return new Promise((resolve, reject) => {
+    rl.question('ID de la tarea a marcar como completada: ', (taskId) => {
+      const task = tasks.find((t) => t.id === parseInt(taskId, 10));
+      if (task) {
+        task.completed = true;
+        console.log(`Tarea ${taskId} marcada como completada.`);
+      } else {
+        console.log('ID de tarea no encontrado.');
+      }
+      resolve();
+    });
   });
 }
 
-function displayMenu() {
-  console.log('\nMenú:');
-  console.log('1. Agregar tarea');
-  console.log('2. Lista de tareas');
-  console.log('3. Marcar tarea completada');
-  console.log('4. Salir');
+//AÑADO FUNCION ELIMINAR TAREAS QUE SE ME OLVIDO EN EL PASADO JEJEJE XD
+function deleteTask() {
+  return new Promise((resolve, reject) => {
+    rl.question('ID de la tarea a eliminar: ', (taskId) => {
+      const index = tasks.findIndex((task) => task.id === parseInt(taskId, 10));
 
-  rl.question('Selecciona una opción: ', (choice) => {
+      if (index !== -1) {
+        tasks.splice(index, 1);
+        console.log(`Tarea ${taskId} eliminada.`);
+      } else {
+        console.log('ID de tarea no encontrado.');
+      }
+      resolve();
+    });
+  });
+}
+
+async function displayMenu() {
+  while (true) {
+    console.log('\nMenú:');
+    console.log('1. Agregar tarea');
+    console.log('2. Lista de tareas');
+    console.log('3. Marcar tarea completada');
+    console.log('4. Eliminar tarea');
+    console.log('5. Salir');
+
+    const choice = await askQuestion('Selecciona una opción: ');
+
     switch (choice) {
       case '1':
-        addTask();
+        await addTask();
         break;
       case '2':
-        listTasks();
+        await listTasks();
         break;
       case '3':
-        markTaskAsCompleted();
+        await markTaskAsCompleted();
         break;
       case '4':
+        await deleteTask();
+        break;
+      case '5':
         console.log('Chauuu.');
         rl.close();
-        break;
+        return;
       default:
         console.log('Opción no válida.');
-        displayMenu();
-        break;
     }
+  }
+}
+
+async function askQuestion(question) {
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
   });
 }
 
-displayMenu();
+(async () => {
+  try {
+    await displayMenu();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    rl.close();
+  }
+})();
